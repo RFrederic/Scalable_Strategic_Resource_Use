08219e4b34f7fcb313afb50deda9cf281cf315ea
Frederic Ren <renfrederic@gmail.com>
2017-09-25 12:55:09 -0700

    adding model study plan section to instructor page
    Also including php scripts in the editor settings

--- handle_instructor.js
+++ handle_instructor.js
@@ -6,6 +6,8 @@ $(window).load(function(){
 	var s5 = document.getElementById("s5");
 	var s6 = document.getElementById("s6");
 	var s7 = document.getElementById("s7");
+	var s8 = document.getElementById("s8");
+	var s9 = document.getElementById("s9");
 	var sl = document.getElementById("sl");
 	var n1 = document.getElementById("n1");
 	var n2 = document.getElementById("n2");
@@ -14,12 +16,16 @@ $(window).load(function(){
 	var n5 = document.getElementById("n5");
 	var n6 = document.getElementById("n6");
 	var n7 = document.getElementById("n7");
+	var n8 = document.getElementById("n8");
+	var n9 = document.getElementById("n9");
 	var b1 = document.getElementById("b1");
 	var b2 = document.getElementById("b2");
 	var b3 = document.getElementById("b3");
 	var b4 = document.getElementById("b4");
 	var b5 = document.getElementById("b5");
 	var b6 = document.getElementById("b6");
+	var b7 = document.getElementById("b7");
+	var b8 = document.getElementById("b8");
 	n1.onclick = function(){
 		s1.className = "hidden";
 		s2.className = "normal";
@@ -46,6 +52,14 @@ $(window).load(function(){
 	}
 	n7.onclick = function(){
 		s7.className = "hidden";
+		s8.className = "normal";
+	}
+	n8.onclick = function(){
+		s8.className = "hidden";
+		s9.className = "normal";
+	}
+	n9.onclick = function(){
+		s9.className = "hidden";
 		sl.className = "normal";
 	}
 	b1.onclick = function(){
@@ -71,5 +85,13 @@ $(window).load(function(){
 	b6.onclick = function(){
 		s7.className = "hidden";
 		s6.className = "normal";
+	}
+	b7.onclick = function(){
+		s8.className = "hidden";
+		s7.className = "normal";
+	}
+	b8.onclick = function(){
+		s9.className = "hidden";
+		s8.className = "normal";
 	}
 })
