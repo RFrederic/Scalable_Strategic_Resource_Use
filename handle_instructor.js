91d249a949086426e0acc849c10e673372f27aaa
Frederic Ren <renfrederic@gmail.com>
2017-09-27 12:16:35 -0700

    Refactoring code for clarity

--- handle_instructor.js
+++ handle_instructor.js
@@ -1,141 +1,45 @@
 $(window).load(function(){
-	var s1 = document.getElementById("s1");
-	var s2 = document.getElementById("s2");
-	var s3 = document.getElementById("s3");
-	var s4 = document.getElementById("s4");
-	var s5 = document.getElementById("s5");
-	var s6 = document.getElementById("s6");
-	var s7 = document.getElementById("s7");
-	var s8 = document.getElementById("s8");
-	var s9 = document.getElementById("s9");
-	var s10 = document.getElementById("s10");
-	var s11 = document.getElementById("s11");
-	var s_td = document.getElementById("teacher_demographics");
-	var s_sd = document.getElementById("student_demographics");
-	var sl = document.getElementById("sl");
-	var n1 = document.getElementById("n1");
-	var n2 = document.getElementById("n2");
-	var n3 = document.getElementById("n3");
-	var n4 = document.getElementById("n4");
-	var n5 = document.getElementById("n5");
-	var n6 = document.getElementById("n6");
-	var n7 = document.getElementById("n7");
-	var n8 = document.getElementById("n8");
-	var n9 = document.getElementById("n9");
-	var n10 = document.getElementById("n10");
-	var n11 = document.getElementById("n11");
-	var n12 = document.getElementById("n12");
-	var n13 = document.getElementById("n13");
-	var b1 = document.getElementById("b1");
-	var b2 = document.getElementById("b2");
-	var b3 = document.getElementById("b3");
-	var b4 = document.getElementById("b4");
-	var b5 = document.getElementById("b5");
-	var b6 = document.getElementById("b6");
-	var b7 = document.getElementById("b7");
-	var b8 = document.getElementById("b8");
-	var b9 = document.getElementById("b9");
-	var b10 = document.getElementById("b10");
-	var b11 = document.getElementById("b11");
-	var b12 = document.getElementById("b12");
-	n1.onclick = function(){
-		s1.className = "hidden";
-		s2.className = "normal";
+	// get sections and buttons
+	var sections = document.getElementsByTagName('section');
+	var next_buttons = document.getElementsByClassName('next_button');
+	var previous_buttons = document.getElementsByClassName('previous_button');
+
+	// define handlers for button clicks
+	function handle_nextB_click(button_index){
+		sections[button_index].className = "hidden";
+		sections[button_index+1].className = "normal";
 	}
-	n2.onclick = function(){
-		s2.className = "hidden";
-		s3.className = "normal";
+
+	function handle_previousB_click(button_index){
+		sections[button_index].className = "normal";
+		sections[button_index+1].className = "hidden";
 	}
-	n3.onclick = function(){
-		s3.className = "hidden";
-		s4.className = "normal";
+
+	// initialize the page and setting up section display properties 
+	// as well as event listeners for button clicks
+	function initialize(){
+		for (var i = 0; i < sections.length; i++) {
+			sections[i].className = "hidden"; 
+		}
+		// display the first section
+		sections[0].className = "normal";
+		// using closure to make sure all buttons are properly set up
+		for (var i = 0; i < next_buttons.length; i++) {
+			(function (i){
+				next_buttons[i].addEventListener('click', function(){
+					handle_nextB_click(i);
+				}, false);
+			})(i);
+		}
+
+		for (var i = 0; i < previous_buttons.length; i++) {
+			(function (i){
+				previous_buttons[i].addEventListener('click', function(){
+					handle_previousB_click(i);
+			}, false);
+			})(i);
+		}
 	}
-	n4.onclick = function(){
-		s4.className = "hidden";
-		s5.className = "normal";
-	}
-	n5.onclick = function(){
-		s5.className = "hidden";
-		s6.className = "normal";
-	}
-	n6.onclick = function(){
-		s6.className = "hidden";
-		s7.className = "normal";
-	}
-	n7.onclick = function(){
-		s7.className = "hidden";
-		s8.className = "normal";
-	}
-	n8.onclick = function(){
-		s8.className = "hidden";
-		s9.className = "normal";
-	}
-	n9.onclick = function(){
-		s9.className = "hidden";
-		s10.className = "normal";
-	}
-	n10.onclick = function(){
-		s10.className = "hidden";
-		s11.className = "normal";
-	}
-	n11.onclick = function(){
-		s11.className = "hidden";
-		s_td.className = "normal";
-	}
-	n12.onclick = function(){
-		s_td.className = "hidden";
-		s_sd.className = "normal";
-	}
-	n13.onclick = function(){
-		s_sd.className = "hidden";
-		sl.className = "normal";
-	}
-	b1.onclick = function(){
-		s2.className = "hidden";
-		s1.className = "normal";
-	}
-	b2.onclick = function(){
-		s3.className = "hidden";
-		s2.className = "normal";
-	}
-	b3.onclick = function(){
-		s4.className = "hidden";
-		s3.className = "normal";
-	}
-	b4.onclick = function(){
-		s5.className = "hidden";
-		s4.className = "normal";
-	}
-	b5.onclick = function(){
-		s6.className = "hidden";
-		s5.className = "normal";
-	}
-	b6.onclick = function(){
-		s7.className = "hidden";
-		s6.className = "normal";
-	}
-	b7.onclick = function(){
-		s8.className = "hidden";
-		s7.className = "normal";
-	}
-	b8.onclick = function(){
-		s9.className = "hidden";
-		s8.className = "normal";
-	}
-	b9.onclick = function(){
-		s10.className = "hidden";
-		s9.className = "normal";
-	}
-	b10.onclick = function(){
-		s11.className = "hidden";
-		s10.className = "normal";
-	}
-	b11.onclick = function(){
-		s_td.className = "hidden";
-		s11.className = "normal";
-	}
-	b12.onclick = function(){
-		s_sd.className = "hidden";
-		s_td.className = "normal";
-	}
+
+	initialize();
 })
